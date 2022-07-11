fdbb9e7b95072907f997c4d70bf7b7c596882bfb
enesgunacti <gnctenes@gmail.com>
2022-07-11 21:47:35 +0300

    Add project files.

---
+++ ASPNETGELISTIRMEKAMP/Controllers/CategoryController.cs
@@ -0,0 +1,42 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPNETGELISTIRMEKAMP.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        CategoryManager categoryManager = new CategoryManager();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetCategoryList()
+        {
+            var categoryValues = categoryManager.GetAllBL();
+            return View(categoryValues);
+        }
+
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            categoryManager.CategoryAddBL(category);
+            return RedirectToAction("GetCategoryList");
+        }
+
+
+    }
+}
---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,35 @@
+﻿using DataAccess.Concrete.Repositories;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repository = new GenericRepository<Category>();
+
+        public List<Category> GetAllBL()
+        {
+            return repository.List();
+        }
+
+        public void CategoryAddBL(Category category)
+        {
+            if (category.CategoryName == "" || category.CategoryName.Length <= 3 ||
+                category.CategoryDescription == "" || category.CategoryName.Length >= 51
+                )
+            {
+                // hata mesajı
+            }
+
+            else
+            {
+                repository.Insert(category);
+            }
+        }
+}
+}
